e3dbce29d9b37e2473164737b4de03c8f8d29a35
Austin <asd30118dd@gmail.com>
2025-09-03 23:56:54 +0800

    抓取博客來銷售排行

--- test.py
+++ test.py
@@ -1,2 +1,8 @@
 import requests
-print(requests.get("https://www.youtube.com/watch"))
+from bs4 import BeautifulSoup
+yt=requests.get("https://www.books.com.tw/web/books/?loc=menu_1_001p")
+print(yt)
+soup=BeautifulSoup(yt.text,"html.parser")
+s=soup.findAll("h4")
+for i in s:
+    print(i.a.string)
