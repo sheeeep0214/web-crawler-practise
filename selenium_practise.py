0b3e4b9acc50f74b4541d7f9321e8335a8723ce8
Austin <asd30118dd@gmail.com>
2025-09-04 21:32:36 +0800

    登入體育場地網站

--- selenium_practise.py
+++ selenium_practise.py
@@ -1,5 +1,12 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 driver=webdriver.Chrome()
-driver.get("https://www.google.com/?hl=zh_TW")
-print(driver.title)
-driver.quit()
+driver.get("https://sys.ndhu.edu.tw/gc/sportcenter/SportsFields/login.aspx")
+user_name=driver.find_element("name","ctl00$MainContent$TxtUSERNO")
+user_name.send_keys("411321218")
+user_password=driver.find_element("name","ctl00$MainContent$TxtPWD")
+user_password.send_keys("30118ddgg")
+user_password.send_keys(Keys.RETURN)
+time.sleep(3)
+#driver.quit()
